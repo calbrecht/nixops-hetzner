38b9c628afec2975ab53d52bc1ec5e9139d125a2
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-24 19:17:08 +0100

    Fallback to python provided by poetry2nix

--- default.nix
+++ default.nix
@@ -4,7 +4,6 @@ let
 in
 pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./.;
-  python = pkgs.python3;
   overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
   meta.description = "Nix package for ${pkgs.stdenv.system}";
 }
