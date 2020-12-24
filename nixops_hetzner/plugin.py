d49f8b2df7468494ee7271a107b7d0d27fafc38f
Christian Albrecht <christian.albrecht@mayflower.de>
2020-12-24 19:16:32 +0100

    Make it compatibel with nixops 2.0 by creating a Plugin class

--- nixops_hetzner/plugin.py
+++ nixops_hetzner/plugin.py
@@ -1,12 +1,18 @@
 import os.path
 import nixops.plugins
+from nixops.plugins import Plugin
+
+
+class NixopsHetznerPlugin(Plugin):
+    @staticmethod
+    def nixexprs():
+        return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
+
+    @staticmethod
+    def load():
+        return ["nixops_hetzner.backends.server"]
 
 
 @nixops.plugins.hookimpl
-def nixexprs():
-    return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
-
-
-@nixops.plugins.hookimpl
-def load():
-    return ["nixops_hetzner.backends.server"]
+def plugin():
+    return NixopsHetznerPlugin()
